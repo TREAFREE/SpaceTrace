fd38e3954e24defb2023e0acf95026f2a8eff67c
my <2261996970@qq.com>
2026-07-18 00:33:16 +0800

    Initial Commit

---
+++ SpaceTrace/SpaceTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTraceApp.swift
+//  SpaceTrace
+//
+//  Created by ma yue on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SpaceTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpaceTraceTests/SpaceTraceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTraceTests.swift
+//  SpaceTraceTests
+//
+//  Created by ma yue on 7/18/26.
+//
+
+import Testing
+@testable import SpaceTrace
+
+struct SpaceTraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
